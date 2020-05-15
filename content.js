a99e4e2c5f8154e616d50d63c0f6e5049ad10178
Missing-Texture <louis.hoeser@gmail.com>
2020-05-16 00:29:19 +0200

    added onClick Genius Api Search added DOM Selection of YT video Title added Title shortening at '(' for way better search results

--- content.js
+++ content.js
@@ -1,13 +1,29 @@
-console.log("Genius TEST!");
-var menuContainer = document.querySelector('#menu-container');
-var menu = document.querySelector('#top-level-buttons');
-//var likeButton = menuContainer[0];
-var likeButton = menuContainer.children[0];
-console.log(menuContainer);
-var button = document.createElement('button');
-//button.innerHTML = "Testing!!!";
-button.className = "custom-button-style";
+const caToken = 'Bearer KiBIJCgvSJ_P-LB_7q1UjnNNoH2f2LlajXVrL3LBFwvWNoAZirXri4ght5sunV36';
+const url='https://api.genius.com/search?q=';
 
-//menuContainer.appendChild(button);
-//menuContainer.insertBefore(button, likeButton);
-menuContainer.parentNode.insertBefore(button, menuContainer)
+const parent = document.querySelector('#info');
+const menuContainer = document.querySelector('#menu-container');
+let title = document.querySelector('.title');
+
+var button = document.createElement('button');
+button.className = "custom-button-style";
+button.onclick = function () {
+    let title = document.querySelector('.title').firstElementChild.innerHTML;
+    
+    let trimmed = title.split('(')[0];
+    console.log(encodeURI(trimmed))
+
+    fetch(url+encodeURI(trimmed), {
+        method : 'GET',
+        mode: 'cors',
+        headers : {
+            'Authorization': caToken,
+        }
+    })
+    .then(data=>{return data.json()})
+    .then(result=>{return result.response.hits[0].result.url})
+    .then(url=>{window.open(url, '_blank');})
+    .catch(error => console.log('error', error));
+}
+
+menuContainer.parentElement.insertBefore(button, menuContainer)
