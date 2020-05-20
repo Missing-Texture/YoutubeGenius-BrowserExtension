e40628b610b961d4d0c808c0fc21f14ad7c16ebd
Missing-Texture <louis.hoeser@gmail.com>
2020-05-20 23:34:00 +0200

    moved logic to background script added a toolbar button

---
+++ background.js
@@ -0,0 +1,31 @@
+const caToken = 'Bearer KiBIJCgvSJ_P-LB_7q1UjnNNoH2f2LlajXVrL3LBFwvWNoAZirXri4ght5sunV36';
+const url='https://api.genius.com/search?q=';
+
+browser.browserAction.onClicked.addListener(handleClick);
+
+function handleClick() {
+    console.log("clicked");
+    browser.tabs.executeScript({
+        file: "./content.js",
+    })
+    .then(onExecuted);
+    
+};
+
+function onExecuted(result) {
+    console.log("received: "+ result)
+
+    fetch(url+encodeURI(result), {
+        method : 'GET',
+        mode: 'cors',
+        headers : {
+            'Authorization': caToken,
+        }
+    })
+    .then(data=>{return data.json()})
+    .then(result=>{return result.response.hits[0].result.url})
+    .then(url=>{browser.tabs.create({
+        url: url
+    })})
+    .catch(error => console.log('error', error));
+}
--- content.js
+++ content.js
@@ -1,29 +1,5 @@
-const caToken = 'Bearer KiBIJCgvSJ_P-LB_7q1UjnNNoH2f2LlajXVrL3LBFwvWNoAZirXri4ght5sunV36';
-const url='https://api.genius.com/search?q=';
+console.log("Content Script geladen")
 
-const parent = document.querySelector('#info');
-const menuContainer = document.querySelector('#menu-container');
-let title = document.querySelector('.title');
-
-var button = document.createElement('button');
-button.className = "custom-button-style";
-button.onclick = function () {
-    let title = document.querySelector('.title').firstElementChild.innerHTML;
-    
-    let trimmed = title.split('(')[0];
-    console.log(encodeURI(trimmed))
-
-    fetch(url+encodeURI(trimmed), {
-        method : 'GET',
-        mode: 'cors',
-        headers : {
-            'Authorization': caToken,
-        }
-    })
-    .then(data=>{return data.json()})
-    .then(result=>{return result.response.hits[0].result.url})
-    .then(url=>{window.open(url, '_blank');})
-    .catch(error => console.log('error', error));
-}
-
-menuContainer.parentElement.insertBefore(button, menuContainer)
+let title = document.querySelector('.title').firstElementChild.innerHTML;
+let trimmed = title.split('(')[0]; 
+trimmed; // needed to pass let as return Value to background.js 
